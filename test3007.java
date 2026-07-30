fa3394643f4e6ffcf2e1f1d797bd2743144a6e48
Anhkhoa1302 <tranmacanhkhoa1302@gmail.com>
2026-07-30 21:13:00 +0700

    Created test3007

---
+++ test3007.java
@@ -0,0 +1,48 @@
+package Kiemtra;
+
+import java.util.Scanner;
+
+public class test3007 {
+    public static void fibonacci(int n) {
+        int a = 0;
+        int b = 1;
+        int c = 0;
+
+        while (a <= n) {
+            System.out.println(a + " ");
+            c = a + b;
+            a = b;
+            b = c;
+        }
+    }
+
+    public static void sodoixung(int m) {
+        int sodao = 0;
+        int kq = 0;
+        int kqss = m;
+        while (m > 0) {
+            sodao = m % 10;
+            kq = kq * 10 + sodao;
+            m = m / 10;
+            if (kq == kqss) {
+                System.out.println(" La so doi xung: " + kqss);
+            } else {
+                System.out.println("Khong phai la so doi xung: " + kqss);
+            }
+        }
+    }
+
+public static void binarysearch(int k)
+    public static void main(String[] args) {
+        System.out.println("Nhap so nguyen: ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+        fibonacci(n);
+        int m = sc.nextInt();
+        sc.nextLine();
+        sodoixung(m);
+
+    }
+
+}
